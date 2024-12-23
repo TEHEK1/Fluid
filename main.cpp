01de68f2a460fae8bea50e2149096d815fd2282f
TEHEK1 <kasapovamir9@gmail.com>
2024-12-23 15:19:37 +0300

    Idk

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 
 using namespace std;
 
+constexpr size_t T = 1'000'000;
+constexpr std::array<pair<int, int>, 4> deltas{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
+
 class Type {
 public:
 };
@@ -43,7 +46,7 @@ private:
     double m_value;
 };
 
-template <int N, int K>
+template <int N = 32, int K = 16>
 class Fixed : public Type {
 public:
     using ValueType = typename std::conditional_t<
@@ -104,7 +107,7 @@ public:
 
 template <typename T1, typename T2>
 T1& operator += (T1& var1, const T2& var2) {
-    var1 = double(var1) + double(var2);
+    var1 = var1 + var2;
     return var1;
 }
 
@@ -115,107 +118,120 @@ T1& operator -= (T1& var1, const T2& var2) {
     return var1;
 }
 
-template <int w, int h>
+template <int h, int w>
 class Size {
 public:
-    static constexpr int width = w;
     static constexpr int height = h;
+    static constexpr int width = w;
+
     static std::string getName() {
-        return "S(" + std::to_string(width) + "," + to_string(height) +")";
+        return "S(" + std::to_string(height) + "," + to_string(width) +")";
     }
 
 };
 
-
 template <typename PType, typename VType, typename VFlowType, int N, int M>
 class Fluid{
 public:
-    static constexpr size_t T = 1'000'000;
-    static constexpr std::array<pair<int, int>, 4> deltas{{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}};
-    int rtN{};
-    int rtM{};
-    std::array<PType, 256> rho{};
     Fluid(const std::vector<std::string> &srcField) {
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M + 1; j++) {
-                field[i][j] = srcField[i][j];
+            for(int j = 0; j < M; j++) {
+                field.p.at(i).at(j) = srcField.at(i).at(j);
             }
+            field.p.at(i).at(M) = '\0';
         }
     };
-    Fluid(int rtN, int rtM): rtN(rtN), rtM(rtM), field(rtN, rtM + 1),
-                             p(rtN, rtM), old_p(rtN, rtM), last_use(rtN, rtM), dirs(rtN, rtM), velocity(rtN, rtM), velocity_flow(rtN, rtM)  {};
 
+// char field[N][M + 1] = {
+//     "#####",
+//     "#.  #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#.# #",
+//     "#...#",
+//     "#####",
+//     "#   #",
+//     "#   #",
+//     "#   #",
+//     "#####",
+// };
     template <typename T, int planeN = N, int planeM = M>
     struct Plane {
         Plane() = default;
         Plane(int rtN, int rtM) : p(rtN, std::vector<T> (rtM)) {};
+        Plane(std::array<std::array<char, planeM>, planeN> srcfield) : p(srcfield) {
+        }
         using ptype = typename std::conditional_t<
                 (N != 0), std::array<std::array<T, planeM>, planeN>,
-        std::vector<std::vector<T>>
+                std::vector<std::vector<T>>
         >;
-
         ptype p;
     };
 
-    Plane<char, N, M + 1> field;
-    Plane<PType> p, old_p;
-    Plane<int> last_use;
-    Plane<int> dirs;
-    template <typename T>
-    struct VectorField {
-        VectorField() = default;
-        VectorField(int rtN, int rtM) : v(rtN, rtM){};
-        Plane<array<T, deltas.size()>> v;
+    Plane<char, N, M+1> field{};
 
-        T &add(int x, int y, int dx, int dy, T dv) {
+
+
+
+    std::array<Fixed<>, 256> rho;
+
+    Plane<PType> p, old_p;
+
+    template <typename Tp>
+    struct VectorField {
+        Plane<array<Tp, deltas.size()>> v;
+        Tp &add(int x, int y, int dx, int dy, Tp dv) {
             return get(x, y, dx, dy) += dv;
         }
 
-        T &get(int x, int y, int dx, int dy) {
+        Tp &get(int x, int y, int dx, int dy) {
             size_t i = ranges::find(deltas, pair(dx, dy)) - deltas.begin();
             assert(i < deltas.size());
-            return v.p[x][y][i];
+            return v.p.at(x).at(y).at(i);
         }
     };
+
     VectorField<VType> velocity{};
-    VectorField<VFlowType>velocity_flow{};
-
-
+    VectorField<VFlowType> velocity_flow{};
+    Plane<int> last_use;
     int UT = 0;
 
 
     mt19937 rnd{1337};
 
-    tuple<VType, bool, pair<int, int>> propagate_flow(int x, int y, VType lim) {
-        last_use.p[x][y] = UT - 1;
-        VType ret = 0;
-        for (auto [dx, dy]: deltas) {
+    tuple<Fixed<>, bool, pair<int, int>> propagate_flow(int x, int y, Fixed<> lim) {
+        last_use.p.at(x).at(y) = UT - 1;
+        Fixed<> ret = 0;
+        for (auto [dx, dy] : deltas) {
             int nx = x + dx, ny = y + dy;
-            if (field.p[nx][ny] != '#' && last_use.p[nx][ny] < UT) {
+            if (field.p.at(nx).at(ny) != '#' && last_use.p.at(nx).at(ny)< UT) {
                 auto cap = velocity.get(x, y, dx, dy);
                 auto flow = velocity_flow.get(x, y, dx, dy);
                 if (flow == cap) {
                     continue;
                 }
                 // assert(v >= velocity_flow.get(x, y, dx, dy));
-                auto vp = min(double(lim), double(cap - flow));
-                if (last_use.p[nx][ny] == UT - 1) {
-                    velocity_flow.add(x, y, dx, dy, vp);
-                    last_use.p[x][y] = UT;
+                auto vp = min(Fixed<>(lim), Fixed<>(cap - flow));
+                if (last_use.p.at(nx).at(ny) == UT - 1) {
+                    velocity_flow.add(x, y, dx, dy, VFlowType(vp));
+                    last_use.p.at(x).at(y) = UT;
                     // cerr << x << " " << y << " -> " << nx << " " << ny << " " << vp << " / " << lim << "\n";
                     return {vp, 1, {nx, ny}};
                 }
                 auto [t, prop, end] = propagate_flow(nx, ny, vp);
                 ret += t;
                 if (prop) {
-                    velocity_flow.add(x, y, dx, dy, VFlowType(double(t)));
-                    last_use.p[x][y] = UT;
+                    velocity_flow.add(x, y, dx, dy, VFlowType(t));
+                    last_use.p.at(x).at(y) = UT;
                     // cerr << x << " " << y << " -> " << nx << " " << ny << " " << t << " / " << lim << "\n";
                     return {t, prop && end != pair(x, y), end};
                 }
             }
         }
-        last_use.p[x][y] = UT;
+        last_use.p.at(x).at(y) = UT;
         return {ret, 0, {0, 0}};
     }
 
@@ -227,9 +243,9 @@ public:
     void propagate_stop(int x, int y, bool force = false) {
         if (!force) {
             bool stop = true;
-            for (auto [dx, dy]: deltas) {
+            for (auto [dx, dy] : deltas) {
                 int nx = x + dx, ny = y + dy;
-                if (field.p[nx][ny] != '#' && last_use.p[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) > 0) {
+                if (field.p.at(nx).at(ny)!= '#' && last_use.p.at(nx).at(ny) < UT - 1 && velocity.get(x, y, dx, dy) > 0) {
                     stop = false;
                     break;
                 }
@@ -238,22 +254,22 @@ public:
                 return;
             }
         }
-        last_use.p[x][y] = UT;
-        for (auto [dx, dy]: deltas) {
+        last_use.p.at(x).at(y) = UT;
+        for (auto [dx, dy] : deltas) {
             int nx = x + dx, ny = y + dy;
-            if (field.p[nx][ny] == '#' || last_use.p[nx][ny] == UT || velocity.get(x, y, dx, dy) > 0) {
+            if (field.p.at(nx).at(ny) == '#' || last_use.p.at(nx).at(ny) == UT || velocity.get(x, y, dx, dy) > 0) {
                 continue;
             }
             propagate_stop(nx, ny);
         }
     }
 
-    VType move_prob(int x, int y) {
-        VType sum = 0;
+    Fixed<> move_prob(int x, int y) {
+        Fixed<> sum = 0;
         for (size_t i = 0; i < deltas.size(); ++i) {
             auto [dx, dy] = deltas[i];
             int nx = x + dx, ny = y + dy;
-            if (field.p[nx][ny] == '#' || last_use.p[nx][ny] == UT) {
+            if (field.p.at(nx).at(ny) == '#' || last_use.p.at(nx).at(ny) == UT) {
                 continue;
             }
             auto v = velocity.get(x, y, dx, dy);
@@ -270,15 +286,15 @@ public:
         PType cur_p;
         array<VType, deltas.size()> v;
 
-        void swap_with(int x, int y, decltype(field) field, decltype(velocity) velocity, decltype(p) p) {
-            swap(field.p[x][y], type);
-            swap(p.p[x][y], cur_p);
-            swap(velocity.v.p[x][y], v);
+        void swap_with(int x, int y, Plane<char, N, M + 1>& field, Plane<PType>& p, VectorField<VType>& velocity) {
+            swap(field.p.at(x).at(y), type);
+            swap(p.p.at(x).at(y), cur_p);
+            swap(velocity.v.p.at(x).at(y), v);
         }
     };
 
     bool propagate_move(int x, int y, bool is_first) {
-        last_use.p[x][y] = UT - is_first;
+        last_use.p.at(x).at(y) = UT - is_first;
         bool ret = false;
         int nx = -1, ny = -1;
         do {
@@ -287,17 +303,17 @@ public:
             for (size_t i = 0; i < deltas.size(); ++i) {
                 auto [dx, dy] = deltas[i];
                 int nx = x + dx, ny = y + dy;
-                if (field.p[nx][ny] == '#' || last_use.p[nx][ny] == UT) {
+                if (field.p.at(nx).at(ny) == '#' || last_use.p.at(nx).at(ny) == UT) {
                     tres[i] = sum;
                     continue;
                 }
                 auto v = velocity.get(x, y, dx, dy);
                 if (v < 0) {
-                    tres[i] = sum;
+                    tres.at(i) = sum;
                     continue;
                 }
                 sum += v;
-                tres[i] = sum;
+                tres.at(i) = sum;
             }
 
             if (sum == 0) {
@@ -310,40 +326,42 @@ public:
             auto [dx, dy] = deltas[d];
             nx = x + dx;
             ny = y + dy;
-            assert(velocity.get(x, y, dx, dy) > 0 && field.p[nx][ny] != '#' && last_use.p[nx][ny] < UT);
+            assert(velocity.get(x, y, dx, dy) > 0 && field.p.at(nx).at(ny) != '#' && last_use.p.at(nx).at(ny) < UT);
 
-            ret = (last_use.p[nx][ny] == UT - 1 || propagate_move(nx, ny, false));
+            ret = (last_use.p.at(nx).at(ny) == UT - 1 || propagate_move(nx, ny, false));
         } while (!ret);
-        last_use.p[x][y] = UT;
+        last_use.p.at(x).at(y) = UT;
         for (size_t i = 0; i < deltas.size(); ++i) {
             auto [dx, dy] = deltas[i];
             int nx = x + dx, ny = y + dy;
-            if (field.p[nx][ny] != '#' && last_use.p[nx][ny] < UT - 1 && velocity.get(x, y, dx, dy) < 0) {
+            if (field.p.at(nx).at(ny) != '#' && last_use.p.at(nx).at(ny) < UT - 1 && velocity.get(x, y, dx, dy) < 0) {
                 propagate_stop(nx, ny);
             }
         }
         if (ret) {
             if (!is_first) {
                 ParticleParams pp{};
-                pp.swap_with(x, y, field, velocity, p);
-                pp.swap_with(nx, ny, field, velocity, p);
-                pp.swap_with(x, y, field, velocity, p);
+                pp.swap_with(x, y, field, p, velocity);
+                pp.swap_with(nx, ny, field, p, velocity);
+                pp.swap_with(x, y, field, p, velocity);
             }
         }
         return ret;
     }
 
+    Plane<int> dirs;
+
     int main() {
         rho[' '] = 0.01;
         rho['.'] = 1000;
-        PType g = 0.1;
+        VType g = 0.1;
 
         for (size_t x = 0; x < N; ++x) {
             for (size_t y = 0; y < M; ++y) {
-                if (field.p[x][y] == '#')
+                if (field.p.at(x).at(y) == '#')
                     continue;
-                for (auto [dx, dy]: deltas) {
-                    dirs.p[x][y] += (field.p[x + dx][y + dy] != '#');
+                for (auto [dx, dy] : deltas) {
+                    dirs.p.at(x).at(y) += (field.p.at(x + dx).at(y + dy) != '#');
                 }
             }
         }
@@ -354,23 +372,23 @@ public:
             // Apply external forces
             for (size_t x = 0; x < N; ++x) {
                 for (size_t y = 0; y < M; ++y) {
-                    if (field.p[x][y] == '#')
+                    if (field.p.at(x).at(y) == '#')
                         continue;
-                    if (field.p[x + 1][y] != '#')
-                        velocity.add(x, y, 1, 0, VType(g));
+                    if (field.p.at(x + 1).at(y) != '#')
+                        velocity.add(x, y, 1, 0, g);
                 }
             }
 
             // Apply forces from p
-            p = old_p;
+            old_p = p;
             for (size_t x = 0; x < N; ++x) {
                 for (size_t y = 0; y < M; ++y) {
-                    if (field.p[x][y] == '#')
+                    if (field.p.at(x).at(y) == '#')
                         continue;
-                    for (auto [dx, dy]: deltas) {
+                    for (auto [dx, dy] : deltas) {
                         int nx = x + dx, ny = y + dy;
-                        if (field.p[nx][ny] != '#' && old_p.p[nx][ny] < old_p.p[x][y]) {
-                            auto delta_p = old_p.p[x][y] - old_p.p[nx][ny];
+                        if (field.p[nx][ny] != '#' && old_p.p[nx][ny] < old_p.p.at(x).at(y)) {
+                            auto delta_p = old_p.p.at(x).at(y) - old_p.p[nx][ny];
                             auto force = delta_p;
                             auto &contr = velocity.get(nx, ny, -dx, -dy);
                             if (contr * rho[(int) field.p[nx][ny]] >= force) {
@@ -380,8 +398,8 @@ public:
                             force -= contr * rho[(int) field.p[nx][ny]];
                             contr = 0;
                             velocity.add(x, y, dx, dy, force / rho[(int) field.p[x][y]]);
-                            p.p[x][y] -= force / dirs.p[x][y];
-                            total_delta_p -= force / dirs.p[x][y];
+                            p.p.at(x).at(y) -= force / dirs.p.at(x).at(y);
+                            total_delta_p -= force / dirs.p.at(x).at(y);
                         }
                     }
                 }
@@ -395,7 +413,7 @@ public:
                 prop = 0;
                 for (size_t x = 0; x < N; ++x) {
                     for (size_t y = 0; y < M; ++y) {
-                        if (field.p[x][y] != '#' && last_use.p[x][y] != UT) {
+                        if (field.p.at(x).at(y) != '#' && last_use.p.at(x).at(y) != UT) {
                             auto [t, local_prop, _] = propagate_flow(x, y, 1);
                             if (t > 0) {
                                 prop = 1;
@@ -408,20 +426,20 @@ public:
             // Recalculate p with kinetic energy
             for (size_t x = 0; x < N; ++x) {
                 for (size_t y = 0; y < M; ++y) {
-                    if (field.p[x][y] == '#')
+                    if (field.p.at(x).at(y) == '#')
                         continue;
-                    for (auto [dx, dy]: deltas) {
+                    for (auto [dx, dy] : deltas) {
                         auto old_v = velocity.get(x, y, dx, dy);
                         auto new_v = velocity_flow.get(x, y, dx, dy);
                         if (old_v > 0) {
                             assert(new_v <= old_v);
-                            velocity.get(x, y, dx, dy) = double(new_v);
+                            velocity.get(x, y, dx, dy) = VType(new_v);
                             auto force = (old_v - new_v) * rho[(int) field.p[x][y]];
-                            if (field.p[x][y] == '.')
+                            if (field.p.at(x).at(y) == '.')
                                 force *= 0.8;
                             if (field.p[x + dx][y + dy] == '#') {
-                                p.p[x][y] += force / dirs.p[x][y];
-                                total_delta_p += force / dirs.p[x][y];
+                                p.p.at(x).at(y) += force / dirs.p.at(x).at(y);
+                                total_delta_p += force / dirs.p.at(x).at(y);
                             } else {
                                 p.p[x + dx][y + dy] += force / dirs.p[x + dx][y + dy];
                                 total_delta_p += force / dirs.p[x + dx][y + dy];
@@ -435,7 +453,7 @@ public:
             prop = false;
             for (size_t x = 0; x < N; ++x) {
                 for (size_t y = 0; y < M; ++y) {
-                    if (field.p[x][y] != '#' && last_use.p[x][y] != UT) {
+                    if (field.p.at(x).at(y) != '#' && last_use.p.at(x).at(y) != UT) {
                         if (random01() < move_prob(x, y)) {
                             prop = true;
                             propagate_move(x, y, true);
@@ -448,11 +466,14 @@ public:
 
             if (prop) {
                 cout << "Tick " << i << ":\n";
-                for (size_t y = 0; y < N; ++y) {
-                    for(size_t x = 0; x < M + 1; ++x) {
-                        cout << field.p[y][x];
+                for (size_t x = 0; x < N; ++x) {
+                    for(size_t y = 0; y < field.p[x].size(); y++) {
+                        if(!x) {
+                            break;
+                        }
+                        cout << field.p.at(x).at(y);
                     }
-                    cout << "\n";
+                    cout <<  "\n";
                 }
             }
         }
@@ -462,7 +483,7 @@ public:
 
 #define SIZES S(36, 84),S(40,40)
 #define S(w, h) Size<w, h>
-#define TYPES FIXED(32,6),FAST_FIXED(30,7),DOUBLE
+#define TYPES FIXED(32,16),FAST_FIXED(30,7),DOUBLE
 #define FIXED(a, b) Fixed<a,b>
 #define FAST_FIXED(a, b) FastFixed<a,b>
 #define DOUBLE Double
@@ -470,7 +491,7 @@ public:
 template <typename PType, typename VType, typename VFlowType, typename CurSize, typename... Sizes>
 void getFluidSize(int width, int height, const std::vector<std::string>& field  ) {
     if(width == CurSize::width && height == CurSize::height) {
-        auto fluid = Fluid<PType, VType, VFlowType, CurSize::width, CurSize::height>();
+        Fluid<PType, VType, VFlowType, CurSize::height, CurSize::width> fluid(field);
         fluid.main();
     }
     if constexpr (sizeof...(Sizes) > 0) {
@@ -540,13 +561,17 @@ int main(int argc, char* argv[]) {
     assert(!fileName.empty());
 
     std::ifstream fin{fileName};
+    if(!fin) {
+        std::cout << "Failed to open " + fileName;
+        exit(EXIT_FAILURE);
+    }
     std::vector<std::string > field;
     std::string line;
-
     while (std::getline(fin, line)) {
         field.push_back(line);
     }
-    getFluid<TYPES>("DOUBLE", "DOUBLE", "DOUBLE", field.at(0).size() - 1, field.size(), field);
+    assert(field.size());
+    getFluid<TYPES>("FIXED(32,16)", "FIXED(32,16)", "FIXED(32,16)", field.at(0).size(), field.size(), field);
 
     return 0;
 }
